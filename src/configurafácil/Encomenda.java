1d197d93760221ac9c8bf04fd3f304d9d46a73f8
anaribeiro142 <a_m_s_ribeiro@hotmail.com>
2018-11-29 09:58:52 +0000

    pg1

---
+++ src/configurafácil/Encomenda.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package configurafácil;
+
+/**
+ *
+ * @author utilizador
+ */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Encomenda {
+    private String carro;
+    private List <String> configuração;
+    
+    public Encomenda() {
+        this.carro = "";
+        this.configuração = new ArrayList<String>();
+    }
+    
+    public Encomenda(String carro, List<String> quotas){
+        this.carro = carro;
+        this.configuração = new ArrayList<String>();
+        if (configuração != null) this.configuração = quotas.stream().collect(Collectors.toList());
+    }
+    
+    public Encomenda(Encomenda e){
+        this.carro = e.getCarro();
+        this.configuração = e.getConfig();
+    }
+    
+    public String getCarro(){
+        return this.carro;
+    }
+    
+    public List<String> getConfig(){
+        return this.configuração.stream().collect(Collectors.toList());
+    }
+    
+    public void addToConfiguração(String c){
+        this.configuração.add(c);
+    }
+    
+    public boolean equals(Object o) {
+        boolean b=false;
+
+        if (o!= null && o instanceof Encomenda) {
+            Encomenda e = (Encomenda)o;
+            b = this.carro.equals(e.getCarro());
+        }
+        return b;
+    }
+    
+    public Object clone() {
+        return new Encomenda(this);
+    }
+
+    //Falta imprimir a lista dos arrays
+    public String toString() {
+        StringBuffer sb = new StringBuffer("Encomenda(");
+        sb.append(this.carro);
+        return sb.toString();
+    }
+}
