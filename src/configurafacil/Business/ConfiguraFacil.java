ec9989fc61e31bf3e41bafedf3a7ad4a74b3d447
FilipaPereira <lipavilela@gmail.com>
2018-12-18 23:39:36 +0000

    componentes

--- src/configurafacil/Business/ConfiguraFacil.java
+++ src/configurafacil/Business/ConfiguraFacil.java
@@ -41,22 +41,66 @@ public class ConfiguraFacil {
     }
     
     public void addComp() {
-        Componente c1 = new Componente("Couro comfort carvão","Estofo",15, null, null);
+        //PINTURA
+        Componente c1 = new Componente("Preto","Pintura",200,null, null);
+        Componente c2 = new Componente("Branco","Pintura",200,null, null);
+        Componente c3 = new Componente("Cinzento","Pintura",200,null, null);
+        Componente c4 = new Componente("Azul","Pintura",200,null, null);
+        //MOTOR
+        Componente c5 = new Componente("D4 190cv Man. 6 Vel.","Motor",3500,null, null);
+        Componente c6 = new Componente("D4 190cv Geatronic 8 Vel.","Motor",3700,null, null);
+        Componente c7 = new Componente("D5 235cv Geatronic 8 Vel.","Motor",4200,null, null);
+        //PNEU
+        Componente c8 = new Componente("Bridgestone Turanza T005 205/55 R16 91V","Pneu",450,null, null);
+        Componente c9 = new Componente("Continental ContiEcoContact 5 205/55 R16 91V","Pneu",450,null, null);
+        Componente c10 = new Componente("Continental PremiumContact 6 205/55 R16 91H","Pneu",500,null, null);
+        Componente c11 = new Componente("Firestone Roadhawk 205/55 R16 91H","Pneu",580,null, null);
+        //JANTE
+        Componente c12 = new Componente("Jantes em liga leve 17''","Jante",270,null, null);
+        Componente c13 = new Componente("Jantes em liga leve 18'' 245/45 R18","Jante",340,null, null);
+        Componente c14 = new Componente("Jantes em liga leve 19'' 255/40 R19","Jante",330,null, null);
+        //ESTOFOS
+        Componente c15 = new Componente("Couro comfort carvão","Estofo",800, null, null);
+        Componente c16 = new Componente("Couro comfort âmbar","Estofo",800, null, null);
+        Componente c17 = new Componente("Couro comfort maroon brown","Estofo",800, null, null);
+        Componente c18 = new Componente("Couro comfort blond","Estofo",800, null, null);
+        //PARA-CHOQUES
+        Componente c19 = new Componente("Pára-choques","Pára-choque",270,null, null);
+        //VIDRO
+        Componente c20 = new Componente("Vidro Escurecido","Vidro",200,null, null);
+        //PACOTES
+        Componente c21 = new Componente("Pacote Comfort","Pacote",1700,null, null);
+        Componente c22 = new Componente("Pacote Sport","Pacote",2100,null, null);
+        //TETO
+        Componente c23 = new Componente("Teto de abrir","Teto",390,null, null);
+        //LUZES
+        Componente c24 = new Componente("Pacotes de luz","Luzes",600, null, null);
         stand.addComponente(c1);
-        Componente c2 = new Componente("Jantes em liga leve 17''","Jante",2,null, null);
         stand.addComponente(c2);
-        Componente c3 = new Componente("D4 190cv Man. 6 Vel.","Motor",2,null, null);
         stand.addComponente(c3);
-        Componente c4 = new Componente("Preto","Pintura",2,null, null);
         stand.addComponente(c4);
-        Componente c5 = new Componente("Bridgestone Turanza T005 205/55 R16 91V","Pneu",2,null, null);
         stand.addComponente(c5);
-        Componente c6 = new Componente("Branco","Pintura",2,null, null);
         stand.addComponente(c6);
-        Componente c7 = new Componente("Vidro Escurecido","Vidro",2,null, null);
         stand.addComponente(c7);
-        Componente c8 = new Componente("Pacote Comfort","Pacote",2,null, null);
         stand.addComponente(c8);
+        stand.addComponente(c9);
+        stand.addComponente(c10);
+        stand.addComponente(c11);
+        stand.addComponente(c12);
+        stand.addComponente(c13);
+        stand.addComponente(c14);
+        stand.addComponente(c15);
+        stand.addComponente(c16);
+        stand.addComponente(c17);
+        stand.addComponente(c18);
+        stand.addComponente(c19);
+        stand.addComponente(c20);
+        stand.addComponente(c21);
+        stand.addComponente(c22);
+        stand.addComponente(c23);
+        stand.addComponente(c24);
+       
+       
     }
         
     public void addEnc() {
