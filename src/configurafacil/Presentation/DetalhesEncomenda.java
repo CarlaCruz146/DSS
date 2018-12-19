d9a39f221e98fdaea4ca43d7391d69218ac42c33
Jéssica Lemos <jessica.andreia96@gmail.com>
2018-12-19 10:18:46 +0000

    upd

--- src/configurafacil/Presentation/DetalhesEncomenda.java
+++ src/configurafacil/Presentation/DetalhesEncomenda.java
@@ -8,7 +8,6 @@ package configurafacil.Presentation;
 import configurafacil.Business.Componente;
 import configurafacil.Business.ConfiguraFacil;
 import configurafacil.Business.Encomenda;
-import java.util.HashMap;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -16,22 +15,24 @@ import javax.swing.table.DefaultTableModel;
  * @author jessica
  */
 public class DetalhesEncomenda extends javax.swing.JDialog {
-    private static ListaEncomendas newe;
+    private ListaEncomendas parent;
     private static ConfiguraFacil configura;
     DefaultTableModel model;
  
     /**
      * Creates new form ConsultarStock2
      */
-    public DetalhesEncomenda(ListaEncomendas parent, boolean modal, ConfiguraFacil c) {
+    public DetalhesEncomenda(javax.swing.JDialog parent, boolean modal, ConfiguraFacil c) {
         super(parent, modal);
         initComponents();
         this.configura = c;
+        this.parent = (ListaEncomendas) parent;
         setLocationRelativeTo(this);
-        preencheDetalhes();    }
+        preencheDetalhes();    
+    }
 
     private void preencheDetalhes(){
-        int n = (Integer) newe.model.getValueAt(newe.row,0);  
+        int n = (Integer) this.parent.model.getValueAt(this.parent.row,0);  
         Encomenda e = configura.getFabrica().getGestaoE().getEncomendas().get(n);
         model =  (DefaultTableModel) jTable1.getModel();
         Object rowData[] = new Object[2];
@@ -146,48 +147,6 @@ public class DetalhesEncomenda extends javax.swing.JDialog {
         // TODO add your handling code here:
         this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(DetalhesEncomenda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DetalhesEncomenda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DetalhesEncomenda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(DetalhesEncomenda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                DetalhesEncomenda dialog = new DetalhesEncomenda(newe, true,configura);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
