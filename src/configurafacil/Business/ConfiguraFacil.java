56e43690d485667c03146d3a35a03ec6d3481b8d
CarlaCruz146 <carla.cruz.247@gmail.com>
2018-12-19 19:06:51 +0000

    Povoamento

--- src/configurafacil/Business/ConfiguraFacil.java
+++ src/configurafacil/Business/ConfiguraFacil.java
@@ -51,18 +51,15 @@ public class ConfiguraFacil {
         Componente c5 = new Componente("D4 190cv Man. 6 Vel.","Motor",3500,null, null);
         Componente c6 = new Componente("D4 190cv Geatronic 8 Vel.","Motor",3700,null, null);
         Componente c7 = new Componente("D5 235cv Geatronic 8 Vel.","Motor",4200,null, null);
-        c7.insereIncompativel("Bridgestone Turanza T005 205/55 R16 91V");
         //PNEU
         Componente c8 = new Componente("Bridgestone Turanza T005 205/55 R16 91V","Pneu",450,null, null);
         Componente c9 = new Componente("Continental ContiEcoContact 5 205/55 R16 91V","Pneu",450,null, null);
         Componente c10 = new Componente("Continental PremiumContact 6 205/55 R16 91H","Pneu",500,null, null);
         Componente c11 = new Componente("Firestone Roadhawk 205/55 R16 91H","Pneu",580,null, null);
-        c10.insereObrigatoria("Jantes em liga leve 19'' 255/40 R19");
         //JANTE
         Componente c12 = new Componente("Jantes em liga leve 17''","Jante",270,null, null);
         Componente c13 = new Componente("Jantes em liga leve 18'' 245/45 R18","Jante",340,null, null);
-        Componente c14 = new Componente("Jantes em liga leve 19'' 255/40 R19","Jante",350,null, null);
-        c12.insereIncompativel("Bridgestone Turanza T005 205/55 R16 91V");
+        Componente c14 = new Componente("Jantes em liga leve 19'' 255/40 R19","Jante",330,null, null);
         //ESTOFOS
         Componente c15 = new Componente("Couro comfort carvão","Estofo",800, null, null);
         Componente c16 = new Componente("Couro comfort âmbar","Estofo",800, null, null);
@@ -76,6 +73,18 @@ public class ConfiguraFacil {
         Componente c23 = new Componente("Teto de abrir","Teto",390,null, null);
         //LUZES
         Componente c24 = new Componente("Pacotes de luz","Luzes",600, null, null);
+        //PACOTE COMFORT
+        Componente c25 = new Componente("Bancos elétricos","Bancos",600, null, null);
+        Componente c26 = new Componente("Ar condicionado de 4 Zonas","Ar Condicionado",600, null, null);
+        Componente c27 = new Componente("Pacotes de Luzes Interiores","Luzes",600, null, null);
+        Componente c28 = new Componente("Espelhos Retrovisores com Mecanismo Anti-Encadeamento","Espelhos",600, null, null);
+
+        //PACOTE SPORT
+        Componente c29 = new Componente("Jantes personalizadas","Jantes",600, null, null);
+        Componente c30 = new Componente("Amortecedores Resistentes","Amortecedores",600, null, null);
+        Componente c31 = new Componente("Spoilers","Spoilers",600, null, null);
+        Componente c32 = new Componente("Escape com 2 ponteiras","Escape",600, null, null);
+      
         stand.addComponente(c1);
         stand.addComponente(c2);
         stand.addComponente(c3);
@@ -98,28 +107,42 @@ public class ConfiguraFacil {
         stand.addComponente(c20);
         stand.addComponente(c23);
         stand.addComponente(c24);
-       
+        stand.addComponente(c25);
+        stand.addComponente(c26);
+        stand.addComponente(c27);
+        stand.addComponente(c28);
+        stand.addComponente(c29);
+        stand.addComponente(c30);
+        stand.addComponente(c31);
+        stand.addComponente(c32);
        
     }
       
     public void addPacote() {
-        ArrayList<Componente> componentes = new ArrayList<>();
-        Componente c1 = new Componente("Preto","Pintura",200,null, null);
-        Componente c2 = new Componente("D4 190cv Geatronic 8 Vel.","Motor",3700,null, null);
-        Componente c3 = new Componente("Firestone Roadhawk 205/55 R16 91H","Pneu",580,null, null);
-        Componente c4 = new Componente("Jantes em liga leve 18'' 245/45 R18","Jante",340,null, null);
-        Componente c5 = new Componente("Couro comfort maroon brown","Estofo",800, null, null);
-        Componente c6 = new Componente("Vidro Escurecido","Vidro",200,null, null);
+        ArrayList<Componente> componentesC = new ArrayList<>();
+        ArrayList<Componente> componentesS = new ArrayList<>();
 
-        componentes.add(c1);
-        componentes.add(c2);
-        componentes.add(c3);
-        componentes.add(c4);
-        componentes.add(c5);
-        componentes.add(c6);
+        Componente c1 = stand.getComponente("Bancos elétricos");
+        Componente c2 = stand.getComponente("Ar condicionado de 4 Zonas");
+        Componente c3 = stand.getComponente("Pacotes de Luzes Interiores");
+        Componente c4 = stand.getComponente("Espelhos Retrovisores com Mecanismo Anti-Encadeamento");
         
-        Pacote p1 = new Pacote("Pacote Comfort",300,componentes);
-        Pacote p2 = new Pacote("Pacote Sport",400,null);
+        Componente c5 = stand.getComponente("Jantes personalizadas");
+        Componente c6 = stand.getComponente("Amortecedores Resistentes");
+        Componente c7 = stand.getComponente("Spoilers");
+        Componente c8 = stand.getComponente("Escape com 2 ponteiras");
+        
+        componentesC.add(c1);
+        componentesC.add(c2);
+        componentesC.add(c3);
+        componentesC.add(c4);
+        componentesS.add(c5);
+        componentesS.add(c6);
+        componentesS.add(c7);
+        componentesS.add(c8);
+        
+        Pacote p1 = new Pacote("Pacote Comfort",300,componentesC);
+        Pacote p2 = new Pacote("Pacote Sport",400,componentesS);
         stand.addPacote(p1);
         stand.addPacote(p2);
     }
