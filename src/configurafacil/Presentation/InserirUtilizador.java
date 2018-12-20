82b88038362ceae9c76d790eef5c316d14cabde8
FilipaPereira <lipavilela@gmail.com>
2018-12-20 16:55:21 +0000

    inserir utilizador corrigido

--- src/configurafacil/Presentation/InserirUtilizador.java
+++ src/configurafacil/Presentation/InserirUtilizador.java
@@ -141,8 +141,8 @@ public class InserirUtilizador extends javax.swing.JDialog {
 
     
     private boolean validaDados() {
-        boolean vazio = this.nome.equals("") || 
-                this.pass.equals("")|| ((checkStand.isSelected() == false) && (checkFabrica.isSelected() == false));
+        boolean vazio = this.nome.getText().equals("") || 
+                this.pass.getText().equals("")|| ((checkStand.isSelected() == false) && (checkFabrica.isSelected() == false));
         if (vazio)
             javax.swing.JOptionPane.showMessageDialog(this, "Por favor preencha os dados.", "Dados incompletos", 0);
                
